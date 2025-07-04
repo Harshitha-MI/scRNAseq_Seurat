8681e2151c8e9601fe773a0bf5526561641af5a3
Harshitha Muddamsetty <harshithamuddamsetty@gmail.com>
2025-07-04 18:51:20 -0400

    Update Batch_effect.R

--- Batch_effect.R
+++ Batch_effect.R
@@ -1,150 +1,115 @@
-## ### BATCH effect
+##-----------------------------
+## BATCH EFFECT CORRECTION: scRNA-seq Pipeline
+##-----------------------------
+
+# Set working directory
 setwd("~/Documents/Drive G/Personal Project/scRNA_tutorial/GSE132771_RAW")
 
-# Loading seurat
-
+# Load Seurat
 library(Seurat)
 
-#Loading the scRNA datafiles
+#-----------------------------
+# Load data
+#-----------------------------
 
-NML1 <- Read10X(data.dir = "./NML_I") 
+NML1 <- Read10X(data.dir = "./NML_I")
 NML2 <- Read10X(data.dir = "./NML_II")
 NML3 <- Read10X(data.dir = "./NML_III")
 
-#to remove a file use the rm function
-rm (IPF)
-#creating a seurat object from the dgmatrix
-
+# Create Seurat objects
 NML1 <- CreateSeuratObject(counts = NML1, min.cells = 3, min.features = 200, project = "NML1")
-NML2 <- CreateSeuratObject(counts = NML2, min.cells= 3, min.features = 200, project ="NML2")
+NML2 <- CreateSeuratObject(counts = NML2, min.cells = 3, min.features = 200, project = "NML2")
 NML3 <- CreateSeuratObject(counts = NML3, min.cells = 3, min.features = 200, project = "NML3")
 
-#Merge all the three datasets
+# Merge datasets
+Merged_NML <- merge(x = NML1, y = c(NML2, NML3), add.cell.ids = c("rep1", "rep2", "rep3"), project = "Merged_NML")
 
-Merged_NML <- merge(x= NML1, y = c(NML2, NML3), add.cell.ids= c("rep1", "rep2", "rep3"), project = "Merged_NML")
+#-----------------------------
+# Quality Control
+#-----------------------------
 
-# The next step after merging the dataset would be to perform quality control
-
-# getting the percentage mitochondrial genes, name the col.name as percent.mt
-
+# Calculate % mitochondrial genes
 Merged_NML <- PercentageFeatureSet(Merged_NML, pattern = "^MT-", col.name = "percent.mt")
-View(Merged_NML@meta.data)
 
-# visualize the variability in the merged data set among the replicates to start with data preprocessing
-# we use violin plot to determine the cutoff values for QC
-
+# Visualize QC metrics
 VlnPlot(Merged_NML, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
-#Quality control
-#remove cells with low feature counts or too high feature counts, too much of mitochondrial genes
+# Filter cells based on QC thresholds
+Merged_NML <- subset(Merged_NML, subset = nFeature_RNA > 500 & nFeature_RNA < 3500 &
+                                   nCount_RNA < 30000 & percent.mt < 10)
 
-Merged_NML<- subset(Merged_NML, subset = nFeature_RNA > 500 & nFeature_RNA < 3500 & 
-                      nCount_RNA <30000 & percent.mt < 10)
-
-# view the data distribution again using violin plot
-
+# Visualize after filtering
 VlnPlot(Merged_NML, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
 
-saveRDS(object = Merged_NML, file = "./Merged_NML.RDS")
+# Save QC-processed object
+saveRDS(Merged_NML, file = "./Merged_NML.RDS")
 
-# Normalize the data
+#-----------------------------
+# Normalization, Variable Features, Scaling
+#-----------------------------
 
-Merged_NML_QC <- NormalizeData(Merged_NML, normalization.method = "LogNormalize", scale.factor = 10000)
+Merged_NML <- NormalizeData(Merged_NML)
+Merged_NML <- FindVariableFeatures(Merged_NML, selection.method = "vst", nfeatures = 2000)
+Merged_NML <- ScaleData(Merged_NML)
 
-# the normalized count matrix is scored in pbmc[["RNA"]]$data
-View(Merged_NML_QC[["RNA"]]$data)
+# Visualize variable features
+top10 <- head(VariableFeatures(Merged_NML), 10)
+VariableFeaturePlot(Merged_NML) + 
+  LabelPoints(points = top10, repel = TRUE)
 
-#Find the features top 2000 genes with high variablity, this is done to reduce the dimentionality of the data
-# The function FindVariableFetures stores these features in the VariableFeature slot in the seurat object
-Merged_NML_QC
+#-----------------------------
+# Dimensionality Reduction
+#-----------------------------
 
-Merged_NML_QC <- FindVariableFeatures(Merged_NML_QC, selection.method = "vst", nfeatures = 2000)
+Merged_NML <- RunPCA(Merged_NML)
+DimPlot(Merged_NML, reduction = "pca")
+ElbowPlot(Merged_NML)
 
-Merged_NML_QC
-# To view the VariableFeatures
-VariableFeatures(Merged_NML_QC)
-VariableFeaturePlot(Merged_NML_QC)
+#-----------------------------
+# Clustering & UMAP/tSNE
+#-----------------------------
 
-#plot gene variance vs gene expression
-top10 <- head(VariableFeatures(Merged_NML_QC),10)
-LabelPoints(plot = VariableFeaturePlot(Merged_NML_QC),points = top10, repel = TRUE)
-# we could use these 2000 genes to perform the downstream analysis.
+Merged_NML <- FindNeighbors(Merged_NML, dims = 1:20)
+Merged_NML <- FindClusters(Merged_NML, resolution = 0.3)
 
-#Scale the data using ScaleData
-Merged_NML_QC <- ScaleData(Merged_NML_QC) #2000 variable features.
+Merged_NML <- RunUMAP(Merged_NML, dims = 1:20)
+DimPlot(Merged_NML, reduction = "umap", label = TRUE, repel = TRUE)
 
-View (Merged_NML_QC[["RNA"]]$scale.data)
+Merged_NML <- RunTSNE(Merged_NML, dims = 1:20)
+DimPlot(Merged_NML, reduction = "tsne", label = TRUE, repel = TRUE)
 
-# We can perform scaling using all genes if the dataset is small.
-#all.genes <- rownames(Merged_NML_QC)
-#Merged_NML_QC <- ScaleData(Merged_NML_QC, features = all.genes)
-# Done with standrad pre-processing workfolw
-#Dimentioanlity reduction
-# Principle component analysis: propcess of computing prinicple component, it's a linear dimentionality reduction
-#technique. It projects each data point onto first few first few PC.
-# By default it runs firts 50 PC
+# Marker gene visualization
+FeaturePlot(Merged_NML, features = c("EPCAM", "CLDN5", "COL1A2", "PTPRC"), cols = c('lightgrey', 'blue'))
 
-Merged_NML_QC <- RunPCA(Merged_NML_QC)
-# Examine and visialize PCA using
-# DimPlot and DimHeatmap
-DimPlot(Merged_NML_QC, reduction = "pca") #+ NoLegend()
-DimPlot(Merged_NML_QC, reduction = "pca", dim = c(1,5))
-DimPlot(Merged_NML_QC, reduction = "pca", dim = c(2,3)) 
+# UMAP grouped by sample
+DimPlot(Merged_NML, reduction = "umap", group.by = "orig.ident")
 
-DimHeatmap(Merged_NML_QC, dims = 1, cells = 500, nfeatures = 30, balanced = TRUE)
+#-----------------------------
+# Integration / Batch Correction
+#-----------------------------
 
-# Two ways to determine the dimentionality of the data set:
-# Jackstraw function and Elbow Plot function
-#Elbow plot to identify pc dims with significant variance
-
-ElbowPlot(Merged_NML_QC)
-
-#### Cell Clustering
-
-# first calculate the distances between the pca vectors for each pair of cells
-Merged_NML_QC<- FindNeighbors(Merged_NML_QC, dims = 1:20)
-#The cluster the cells
-Merged_NML_QC <- FindClusters(Merged_NML_QC, resolution = 0.3)
-
-#### Non-linear dimentional reduction technique (UMAP/tSNE)
-
-Merged_NML_QC <- RunUMAP( Merged_NML_QC, dims = 1:20) # use the same pc that we used for find neighbors
-
-DimPlot(Merged_NML_QC, reduction = "umap", label = TRUE,repel = TRUE)
-
-Merged_NML_QC <- RunTSNE(object = Merged_NML_QC)
-
-DimPlot(Merged_NML_QC, reduction = "tsne", label = TRUE, repel = TRUE)
-
-# Epithelial, Endothelial, mesnchymal, immune cells repsectively.
-FeaturePlot(Merged_NML_QC, features = c("EPCAM", "CLDN5", "COL1A2", "PTPRC"), 
-            cols = c('lightgrey','blue'))
-
-#UMAP grouped by the orig.ident
-
-plot1 <- DimPlot(Merged_NML_QC, reduction ="umap", group.by = "orig.ident")
-
-# Reading in the RDS dataset without pre-processing step. QC done
-
+# Load pre-QC merged data
 Merged_NML <- readRDS("./Merged_NML.RDS")
 
-## Data Integration
-#ifnb[["RNA"]] <- split(ifnb[["RNA"]], f = ifnb$stim)
 Merged_NML <- NormalizeData(Merged_NML)
 Merged_NML <- FindVariableFeatures(Merged_NML)
 Merged_NML <- ScaleData(Merged_NML)
 Merged_NML <- RunPCA(Merged_NML)
 
-Merged_NML <- IntegrateLayers(object = Merged_NML, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca",
-                        verbose = FALSE)
+# CCA integration
+Merged_NML <- IntegrateLayers(Merged_NML, method = CCAIntegration, orig.reduction = "pca", new.reduction = "integrated.cca", verbose = FALSE)
 
-# re-join layers after integration
+# Re-join layers
 Merged_NML[["RNA"]] <- JoinLayers(Merged_NML[["RNA"]])
 
+# Clustering and UMAP on integrated data
 Merged_NML <- FindNeighbors(Merged_NML, reduction = "integrated.cca", dims = 1:20)
 Merged_NML <- FindClusters(Merged_NML, resolution = 0.3)
 Merged_NML <- RunUMAP(Merged_NML, dims = 1:20, reduction = "integrated.cca")
-plot2 <- DimPlot(Merged_NML, reduction ="umap", group.by = "orig.ident")
-plot1 + plot2
+
+# Plot UMAP before and after integration
+p1 <- DimPlot(Merged_NML, reduction = "umap", group.by = "orig.ident")
+p2 <- DimPlot(Merged_NML, reduction = "umap", group.by = "orig.ident")
+p1 + p2
 
 
